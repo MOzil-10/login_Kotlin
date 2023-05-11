53902edf16301c10940a529c800366261638853e
Mukosi Budeli <trevornkuna7@gmail.com>
2023-05-11 15:12:12 +0200

    space

--- app/src/main/java/com/example/springboot_application/User.java
+++ app/src/main/java/com/example/springboot_application/User.java
@@ -3,7 +3,6 @@ package com.example.springboot_application;
 public class User {
     private String userName;
     private String password;
-
     public User(String userName, String password) {
         this.userName = userName;
         this.password = password;
